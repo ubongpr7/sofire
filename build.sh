4906cc16da7c2329660dd38f6bd8084a5c6b6c84
ubongpr7 <ubongpr7@gmail.com>
2024-11-17 10:48:43 -0800

    vercel setup

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
-
 #!/bin/bash
 
-# Build the project
-echo "Building the project..."
+# Install pip
+echo "Installing pip..."
+python3.9 -m ensurepip --upgrade || true
+python3.9 -m pip install --upgrade pip setuptools wheel
+
+# Install dependencies
+echo "Installing dependencies..."
 python3.9 -m pip install -r requirements.txt
 
+# Django migrations
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
+# Collect static files
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
