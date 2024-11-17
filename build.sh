a28c181b51c57047e37abfa7d1ff36785ba5fb2d
ubongpr7 <ubongpr7@gmail.com>
2024-11-17 10:44:03 -0800

    vercel setup

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
-# build the project
-echo "Building the project"
-python3.9 -m pip3 install -r requirements.txt
 
-echo "Makemigrations..."
+#!/bin/bash
+
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-echo "Collecting staticfiles"
+echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
