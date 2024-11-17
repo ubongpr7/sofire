5fba5b124efb55981a4d3c96c7c4a0dd8099cf0b
ubongpr7 <ubongpr7@gmail.com>
2024-11-17 11:35:11 -0800

    vercel setup

--- build.sh
+++ build.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-# Install pip3
-echo "Installing pip3..."
-sudo apt-get update -y
-sudo apt-get install -y python3-pip
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
 
-# Upgrade pip3, setuptools, and wheel
-echo "Upgrading pip3 and related tools..."
-pip3 install --upgrade pip setuptools wheel
-
-# Install dependencies
-echo "Installing dependencies..."
-pip3 install -r requirements.txt
-
-# Django migrations
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-# Collect static files
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
