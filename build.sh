1efc057b0cc81556e9d0136cb9f1c04c42fa44be
ubongpr7 <ubongpr7@gmail.com>
2024-11-17 10:37:02 -0800

    db setup

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # build the project
 echo "Building the project"
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip3 install -r requirements.txt
 
 echo "Makemigrations..."
 python3.9 manage.py makemigrations --noinput
