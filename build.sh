ef10f7d3a18bbc33dc331a69b927b7110e0788eb
ubongpr7 <ubongpr7@gmail.com>
2024-11-17 11:40:06 -0800

    vercel setup

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
