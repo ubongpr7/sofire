4379ee7ea3cf636c6c875028dfe1d2f25d3afb18
ubongpr7 <ubongpr7@gmail.com>
2024-11-21 07:45:17 -0800

    fixes

--- mainapps/common/admin.py
+++ mainapps/common/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 
-for model in [UserInterest,OptionType,AnswerType,CustomForm,FormQuestion,Option,ShortAnswer,EssayAnswer,Attachment,File,Reaction,Tag,Comment,Share,Like,PlatformSettings,Subscription,CustomCategory,SocialMediaHandles]:
+for model in [UserInterest,Attachment,File,Tag,Share,PlatformSettings,Subscription,CustomCategory,SocialMediaHandles]:
     admin.site.register(model)
